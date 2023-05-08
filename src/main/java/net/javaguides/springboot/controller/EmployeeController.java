5344af375f0b28e92b9f61b801cbf502c50e8cc0
Yevhenii Kuzmenko <zheniakuzma@gmail.com>
2023-05-08 10:35:56 +0300

    Created "add employee rest api"

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package net.javaguides.springboot.controller;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +14,14 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    // get all employees
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee)
+    {
+        return employeeRepository.save(employee);
+    }
 }
